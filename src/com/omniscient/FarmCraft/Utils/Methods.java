c77e8ab67123a59bdc0f6724785da6c83a59043d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 02:56:02 -0300

    Main class

---
+++ src/com/omniscient/FarmCraft/Utils/Methods.java
@@ -0,0 +1,14 @@
+package com.omniscient.FarmCraft.Utils;
+
+import com.omniscient.FarmCraft.FarmCraft;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+public class Methods {
+    public static String color(String string){
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
+    public static void consoleLog(String string){
+        Bukkit.getConsoleSender().sendMessage(color(FarmCraft.PREFIX + " &f" + string));
+    }
+}
