432179731cfb023a80632a9646035161a9e9f0ff
BuildTools <unconfigured@null.spigotmc.org>
2020-10-19 03:00:28 -0300

    Main class

--- src/com/omniscient/FarmCraft/FarmCraft.java
+++ src/com/omniscient/FarmCraft/FarmCraft.java
@@ -1,12 +1,13 @@
 package com.omniscient.FarmCraft;
 
+import com.omniscient.FarmCraft.Utils.Methods;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FarmCraft extends JavaPlugin {
     public static String PREFIX = "&e[&fFarmCraft&e]";
 
     public void onEnable(){
-
+        Methods.consoleLog("&aPlugin inicializado.");
     }
     public void onDisable(){
 
